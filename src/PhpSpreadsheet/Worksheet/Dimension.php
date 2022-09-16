b47b0828d720cbad05eab85010bd9dc9618793d1
Paul Mehrer <p.mehrer@metaways.de>
2022-09-16 14:38:14 +0200

    fix Worksheet/Dimension::setXfIndex type

--- src/PhpSpreadsheet/Worksheet/Dimension.php
+++ src/PhpSpreadsheet/Worksheet/Dimension.php
@@ -125,7 +125,7 @@ abstract class Dimension
      *
      * @return $this
      */
-    public function setXfIndex(?int $pValue)
+    public function setXfIndex(?int $XfIndex)
     {
         $this->xfIndex = $XfIndex;
 
