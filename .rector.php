7c76e28a3a0e4fc9c26b9e6856c1cfaa7102418c
Paul Mehrer <p.mehrer@metaways.de>
2026-02-23 12:16:50 +0100

    added rector dev dependency

---
+++ .rector.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\CodeQuality\Rector as CodeQuality;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\ValueObject\PhpVersion;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src/',
+    ])
+    ->withRules([
+        Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector::class,
+    ])
+    ->withPhpVersion(PhpVersion::PHP_84);
